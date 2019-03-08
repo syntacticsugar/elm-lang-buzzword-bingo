759eccaf3a648dddb0521d7e46738c9deb6c6040
Saerom Hong <hello@romcreative.com>
2019-03-08 17:36:04 -0500

    Eugh, as always, a stupid mistake.  Entry.id

--- bingo.elm
+++ bingo.elm
@@ -35,9 +35,9 @@ initialModel =
 initialEntries : List Entry
 initialEntries =
     [ Entry 1 "Future-Proof" 100 False
-    , Entry 1 "Parallel Processing" 200 False
-    , Entry 1 "React Redux JS" 200 False
-    , Entry 1 "Blockchain Startup" 400 False
+    , Entry 2 "Parallel Processing" 200 False
+    , Entry 3 "React Redux JS" 200 False
+    , Entry 4 "Blockchain Startup" 400 False
     ]
 
 
