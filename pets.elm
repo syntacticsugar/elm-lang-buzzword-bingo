bf069ab89eab949198e34edc3bede1d34c275863
Saerom Hong <hello@romcreative.com>
2019-04-13 15:11:43 -0400

    Starts creating record, I need to finish

--- pets.elm
+++ pets.elm
@@ -11,12 +11,26 @@ import Html.Events exposing (onClick)
 
 
 type alias Model =
-    { count : Int }
+    { redFoxCount : Int
+    , goldenRetrieverCount : Int
+    , doveCount : Int
+    , goldfishCount : Int
+    , hedgehogCount : Int
+    , luisCount : Int
+    , count : Int
+    }
 
 
 initialModel : Model
 initialModel =
-    { count = 0 }
+    { redFoxCount = 0
+    , goldenRetrieverCount = 0
+    , doveCount = 0
+    , goldfishCount = 0
+    , hedgehogCount = 0
+    , luisCount = 0
+    , count = 0
+    }
 
 
 type Msg
@@ -49,10 +63,13 @@ view model =
         , div [ class "intro" ] [ text "Vote for your favorite pet" ]
 
         --, div [class "score"] [ text "Score: " ]
-        , span [ class "score" ] [ text "SCORE: " ]
+        --, span [ class "score" ] [ text "SCORE: " ]
         , span [ class "score-integer" ] [ text (String.fromInt model.count) ]
         , ol [ id "pets", class "pink-text" ]
-            [ li [] [ button [ onClick Increment ] [ text "red fox" ] ]
+            [ li []
+                [ button [ onClick Increment ] [ text "red fox" ]
+                , span [] [ text (String.fromInt model.redFoxCount) ]
+                ]
             , li [] [ button [ onClick Increment ] [ text "golden retriever" ] ]
             , li [] [ button [ onClick Increment ] [ text "dove" ] ]
             , li [] [ button [ onClick Increment ] [ text "goldfish" ] ]
